a5e8c3a28ea4e23cb47c333fe5abfc5cd4d54937
Will G <wgrout87@gmail.com>
2023-07-15 10:37:45 -0600

    store controller index

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let controllerIndex = null;
+
 window.addEventListener("gamepadconnected", (event) => {
     handleConnectDisconnect(event, true);
 });
@@ -7,5 +9,19 @@ window.addEventListener("gamepaddisconnected", (event) => {
 });
 
 function handleConnectDisconnect(event, connected) {
-    console.log(connected)
+    const controllerAreaNotConnected = document.getElementById("controller-not-connected-area");
+    const controllerAreaConnected = document.getElementById("controller-connected-area");
+
+    const gamepad = event.gamepad;
+    console.log(gamepad);
+
+    if (connected) {
+        controllerIndex = gamepad.index;
+        controllerAreaNotConnected.style.display = "none";
+        controllerAreaConnected.style.display = "block";
+    } else {
+        controllerIndex = null;
+        controllerAreaNotConnected.style.display = "block";
+        controllerAreaConnected.style.display = "none";
+    }
 }
