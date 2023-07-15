6ff422cb408f43b8108f20197d61169c763c6389
Will G <wgrout87@gmail.com>
2023-07-15 10:49:01 -0600

    dynamically create buttons for connected controller

--- index.js
+++ index.js
@@ -19,9 +19,39 @@ function handleConnectDisconnect(event, connected) {
         controllerIndex = gamepad.index;
         controllerAreaNotConnected.style.display = "none";
         controllerAreaConnected.style.display = "block";
+        createButtonLayout(gamepad.buttons);
     } else {
         controllerIndex = null;
         controllerAreaNotConnected.style.display = "block";
         controllerAreaConnected.style.display = "none";
     }
-}
+};
+
+function createButtonLayout(buttons) {
+    const buttonArea = document.getElementById("buttons");
+    buttonArea.innerHTML = "";
+    for (let i = 0; i < buttons.length; i++) {
+        buttonArea.innerHTML += createButtonHtml(i, 0);
+    }
+};
+
+function createButtonHtml(index, value) {
+    return `<div class="button" id"button-${index}">
+                <svg width="10px" height="50px">
+                    <rect width="10px" height="50px" fill="grey"></rect>
+                    <rect
+                        class="button-meter"
+                        width="10px"
+                        x="0"
+                        y="50"
+                        data-original-y-position="50"
+                        height="50px"
+                        fill="rgb(60, 61, 60)"
+                    ></rect>
+                </svg>
+                <div class="button-text-area">
+                    <div class="button-name">B${index}</div>
+                    <div class="button-value">${value.toFixed(2)}</div>
+                </div>
+            </div>`;
+};
